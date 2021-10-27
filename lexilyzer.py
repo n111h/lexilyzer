37e8ed768d53f9ec80a5ec001558dea6e797e660
n111h <noh@groot.localdomain>
2021-10-27 08:06:42 -0400

    added griffins two-pass fix

--- lexilyzer.py
+++ lexilyzer.py
@@ -138,6 +138,7 @@ def lexilyzerTwoPass(programString = '', filepath = "test.pl0"):##  this lexilyz
     if(not programString):                                      #   if there is no passed program string, a file will be opened
         programString = openFile(filepath)
 
+    orgProgram = programString
     programString = preprocessor(programString)                 #   adds whitespace to program string before .split()
 
     programTokens = programString.split()                       #   splits program string into tokens on arbitary whitespace
@@ -146,17 +147,150 @@ def lexilyzerTwoPass(programString = '', filepath = "test.pl0"):##  this lexilyz
     for token in programTokens:                                 #   iterate through tokens
         if(token in tokens):                                    #   if the token is in the token table
             tokenList.append(str(tokens[token]))                #   add the string value of the looked up token to token string
-        elif(token[0].isalpha()):                               #   else if the token starts with an alpha char
+        elif(token[0].isalpha() or token[0] == "$"):            #   else if the token starts with an alpha char
+            if len(token) > 30:                                 ##  DEBUGGING  ##
+                print("length of the identifier: ", token, " is more than 30 characters.")
+                print("identifier length is ", len(token), " characters")
+                exit()
+            
+
             tokenList.append("0 "+str(hasher(token)))           #   it is a symbol - hash the token and add it to token string
-            symbolTable[hasher(token)].append(token)                                                           ##  DEBUGGING  ##
+            symbolTable[hasher(token)].append(token)
         else:                                                   #   else the token is a number
             tokenList.append("1 "+str(token))                   #   add the number as a prefixed string to token string
+
+    orgProgram = orgProgram.split("\n")
+    lineNum = 1
+    tokenString = ""
+    tokenLines = []
+
+    for i in tokenList:
+        tokenString += i + " * "
+        if i == str(34) or i == str(35):
+            tokenLines.append(tokenString[:-2])
+            tokenString = ""
+
+    for line in orgProgram:
+        print(lineNum)
+        print("The input statement is: ", line.strip())
+        print("The tokens are: ", tokenLines[lineNum - 1])
+        lineNum += 1
     
     return(programTokens,tokenList)                             #   returns: tokens, transcribed (translated) tokens
 
 
-def lexilyzerOnePass(programString = '', filepath = "test.pl0"):##  this lexilyzer passes over the program string once 
-    pass
+def lexilyzerOnePass(programString = '', filepath = "test.pl0"):##  this lexilyzer passes over the program string once
+
+    if(not programString):
+        programString = openFile(filepath)
+
+    tokenList = []
+    token,tokenBuffer,programTokens = '','',''
+    tokenIsToken,tokenIsNumeric,tokenIsIdentifier,tokenIsComplete = False,False,False,False
+    currentLine,currentLineNumber,currentLineTokens,currentLineComplete = '',1,[],False
+
+    for e,char in enumerate(programString):                     #   char = each character in input program, e = its index
+
+        if(tokenIsComplete):                                    #   END TOKEN
+
+            if(tokenIsNumeric+tokenIsToken+tokenIsIdentifier == 1):         #   token is unambiguous
+                programTokens.append(token)
+                if(tokenIsNumeric):
+                    tokenList.append("1 "+token)
+                    tokenIsNumeric = False
+                elif(tokenIsToken):
+                    tokenList.append(str(tokens[token]))
+                    tokenIsToken = False
+                elif(tokenIsIdentifier):
+                    tokenList.append("0 "+str(hasher(token)))
+                    tokenIsIdentifier = False
+            else:                                               #   token is ambiguous (has multiple types - should not happen)
+                print(f"!!!!  TOKEN # {len(tokenList)+1}: '{token}' APPEARS TO BE MULTIPLE TYPES OF TOKEN  !!!!")
+            token = ''
+            prunedTokenTree = {}
+
+        if(currentLineComplete):                                #   END LINE  (debugging printed to STDOUT per documentation)
+            print(f"{'Program line number:'<32}{currentLineNumber}")                                           ##  DEBUGGING  ##
+            print(f"{'The input statement is:'<32}{currentLine}")                                              ##  DEBUGGING  ##
+            print(f"{'The tokens are:'<32}{' * '.join(currentLineTokens)}\n")                                  ##  DEBUGGING  ##
+            currentLineNumber += 1                              #   iterate line counter
+            currentLine,currentLineTokens,currentLineComplete = '',[],False #   clear line specific variables
+        
+        if(len(tokenBuffer) == 0):                              #   START TOKEN
+
+            if(char in symbolTokenTree):                        #   token begins with valid symbol character
+                tokenBuffer += char
+                tokenIsToken = True
+                prunedTokenTree = symbolTokenTree[char]         #   prune token tree to possible tokens
+                if(len(prunedTokenTree)==1):                    #   token is valid single character token
+                    token = tokenBuffer
+                    tokenBuffer = ''
+                    tokenIsComplete = True
+            elif(char.isnumeric()):                             #   token begins with a number
+                tokenBuffer += char
+                tokenIsNumeric = True
+            elif((char.isalpha()) or (char == '$')):            #   token begins with an alpha character
+                if(char in tokenTree):                          #   token could be a token (reserved word)
+                    tokenBuffer += char
+                    tokenIsToken = True
+                    prunedTokenTree = tokenTree[char]           #   prune token tree to possible tokens
+                else:                                           #   token must be an identifier
+                    tokenBuffer += char
+                    tokenIsIdentifier = True
+            elif(char in [' ','\t','\n']):                      #   token begins with whitespace
+                if(char == '\n'):
+                    token = "EOL"                               #   if character is newline add end of line token
+                    tokenBuffer = ''
+                    tokenIsComplete = True
+                    currentLineComplete = True
+                else: pass                                      #   whitespace (expect newline) is ignored
+            else:                                               #   token begins with an unexpected/invalid character
+                print(f"!!!!  TOKEN # {len(tokenList)+1} BEGINS WITH INVALID '{char}' CHARACTER  !!!!")
+
+        elif(len(tokenBuffer) <= 30):                           #   MID TOKEN
+
+            if(tokenIsNumeric):
+                if(char in symbolTokenTree):
+                    token = tokenBuffer
+                    tokenIsComplete = True
+                elif(char.isnumeric()):
+                    tokenBuffer += char
+                elif(char.isalpha()):
+                    pass
+                elif(char in [' ','\t','\n']):
+                    pass
+                else:
+                    print(f"!!!!  TOKEN # {len(tokenList)+1} CONTAINS THE INVALID CHARACTER '{char}'  !!!!")
+            elif(tokenIsToken):
+                if(char in symbolTokenTree):
+                    pass
+                elif(char.isnumeric()):
+                    pass
+                elif(char.isalpha()):
+                    pass
+                elif(char in [' ','\t','\n']):
+                    pass
+                else:
+                    print(f"!!!!  TOKEN # {len(tokenList)+1} CONTAINS THE INVALID CHARACTER '{char}'  !!!!")
+            elif(tokenIsIdentifier):
+                if(char in symbolTokenTree):
+                    pass
+                elif(char.isnumeric()):
+                    pass
+                elif(char.isalpha()):
+                    pass
+                elif(char in [' ','\t','\n']):
+                    pass
+                else:
+                    print(f"!!!!  TOKEN # {len(tokenList)+1} CONTAINS THE INVALID CHARACTER '{char}'  !!!!")
+
+        else:                                                   #   token buffer is greater than max identifier size
+            print(f"!!!!  TOKEN # {len(tokenList)+1}'S BUFFER IS: {len(tokenBuffer)} CHARACTERS LONG  !!!!")
+
+    programTokens.append("EOF")                                 #   add end of file token
+    tokenList.append(str(tokens["EOF"]))                        #   add end of file token value
+
+    return(programTokens,tokenList)                             #   returns: tokens, transcribed (translated) tokens
 
 
 def lexilyzer(programString = '', filepath = "test.pl0"):       ##  this function wraps either lexilyzer (syntax analyzer calls)
